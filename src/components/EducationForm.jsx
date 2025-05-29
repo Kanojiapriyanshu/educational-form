e3df8185a83184ff333c0be65d948d55fd6055ae
user.name:"priyanshukanojia907@gmail.com <priyanshukanojia907@gmail.com>
2025-05-29 22:43:27 +0530

    Fixed API URL env config

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -70,10 +70,11 @@ const UniversityForm = () => {
       });
 
       // Send to backend
-      const response = await fetch('http://localhost:5000/send', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/send`, {
         method: 'POST',
         body: formData,
       });
+      
 
       const data = await response.json();
 
@@ -271,6 +272,7 @@ const UniversityForm = () => {
                 className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               >
                 <option className="bg-white text-gray-700">Country</option>
+                <option value="USA" className="bg-white text-gray-700 hover:bg-gray-100">India</option>
                 <option value="USA" className="bg-white text-gray-700 hover:bg-gray-100">United States</option>
                 <option value="Canada" className="bg-white text-gray-700 hover:bg-gray-100">Canada</option>
                 <option value="UK" className="bg-white text-gray-700 hover:bg-gray-100">United Kingdom</option>
